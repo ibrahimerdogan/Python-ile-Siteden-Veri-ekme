9a8b5f4b1a586526d163b4f8eddcb7c93a193977
İbrahim Erdoğan <info@ibrahimerdogan.com.tr>
2018-04-17 10:08:03 +0300

    vericek.py
    Python ile sitelerden veri çekip mysql veri tabanına veri gönderme işlemi

---
+++ vericek.py
@@ -0,0 +1,38 @@
+import urllib
+import re
+import requests
+import MySQLdb
+import os
+import argparse
+from bs4 import BeautifulSoup
+from array import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url","-u", help="URL Adresi Yaziniz (http://example.com)")
+
+args = parser.parse_args()
+url = args.url
+
+#url    = raw_input("Taranacak bir websitesi giriniz : ")
+r  = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data)
+
+description = soup.find_all('meta',attrs={'name':'description'})
+keywords = soup.find_all('meta',attrs={'name':'keywords'})
+title = soup.find_all('title')
+
+for desc in description:
+    for key in keywords:
+        for tt in title:
+
+            db = MySQLdb.connect(host="localhost",user="root", passwd="root", db="root")
+    db.set_character_set('utf8')
+    insert_stmt = (
+      "INSERT INTO icerik (url, description, keywords, title) "
+      "VALUES (%s, %s, %s, %s)"
+    )
+    data=(url,desc,key,tt)
+    cur = db.cursor()
+    cur.execute(insert_stmt, data)
+    db.commit()
